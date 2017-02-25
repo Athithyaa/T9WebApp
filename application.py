1fad1bc3cdb3c3d7832f995a851f6b35db4b772e
Athithyaa <athithyapanchapakesan@gmail.com>
2017-02-25 16:10:34 -0700

    removing error write to file

--- application.py
+++ application.py
@@ -54,17 +54,6 @@ def internal_error(error):
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
-
 if __name__ == "__main__":
     init_db()
     app.debug = True
